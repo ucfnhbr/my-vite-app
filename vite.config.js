22b25bf54bd98c32e1b2cbe205ceda07a620a6f4
ucfnbr <ucfnbr@ucl.ac.uk>
2026-02-28 16:24:26 +0000

    v1

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -13,7 +11,7 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
-  base: process.env.NODE_ENV === 'production' ? '/my-vite-app/' : '/',
+  base: '/my-vite-app/',  
 })
 
 
